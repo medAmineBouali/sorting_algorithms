e17d580d5fa1fbccf45a70dcbbd624cb31fcfa65
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2024-02-17 17:33:09 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,7 @@ void bubble_sort(int *array, size_t size)
     int temp = 0;
     char bubble = 1;
     size_t j = 0;
+
     while (bubble)
     {
         bubble = 0;
@@ -26,10 +27,6 @@ void bubble_sort(int *array, size_t size)
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
                 print_array(array, size);
-            }
-            else if(j == size - 1)
-            {
-                size --;
             }
         }
     }
