4ffea8f72aec3a4cbbea672535cbf1c7cd84061a
medAmineBouali <93989046+medAmineBouali@users.noreply.github.com>
2024-02-19 16:01:18 +0100

    quick sort

--- sort.h
+++ sort.h
@@ -26,4 +26,5 @@ void switch_nodes(listint_t **list, listint_t **p);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
